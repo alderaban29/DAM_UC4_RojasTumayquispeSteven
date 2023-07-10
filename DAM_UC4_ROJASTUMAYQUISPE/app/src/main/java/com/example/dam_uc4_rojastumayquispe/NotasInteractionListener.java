b272598482559512cf02e55d74a75f9638634b8c
Steven Rojas Tumayquispe <1913110180@untels.edu.pe>
2023-07-10 16:01:36 -0500

    segundo commit

---
+++ DAM_UC4_ROJASTUMAYQUISPE/app/src/main/java/com/example/dam_uc4_rojastumayquispe/NotasInteractionListener.java
@@ -0,0 +1,7 @@
+package com.example.dam_uc4_rojastumayquispe;
+
+public interface NotasInteractionListener {
+    void editNotaClick(Nota nota);
+    void eliminaNotaClick (Nota nota);
+    void favoritaNotaClick (Nota nota);
+}
